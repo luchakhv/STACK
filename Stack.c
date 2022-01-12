f18494ea40e2303279335b7a73e7510e8f9ff281
luchakhv <96920642+luchakhv@users.noreply.github.com>
2022-01-12 03:57:01 +0300

    FIrst version (with standart static array)

---
+++ Stack.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define STACK_MAX_ITEMS 50
+typedef struct stack {
+    int elements[STACK_MAX_ITEMS];
+    int len;
+} Stack;
+
+void Push(Stack *s, int n);
+
+int Pop(Stack *s);
+
+int Top(const Stack *s);
+
+_Bool IsStackEmpty(const Stack *s);
+
+int main() {
+    srand((unsigned int) time(0));
+    Stack s;
+    s.len = 0;//initialising stack
+    for (int i = 0; i < 10; i++) {
+        Push(&s, rand() % 100);
+    }
+    for (int i = 0; i < 10; i++) {
+        printf("%d : \n", i);
+        printf("%d\n", Top(&s));
+        printf("%d\n", Pop(&s));
+    }
+
+
+    return 0;
+}
+
+void Push(Stack *s, const int n) {
+    if (s->len >= STACK_MAX_ITEMS)
+        exit(EXIT_FAILURE);
+    s->elements[s->len] = n;
+    s->len++;
+}
+
+int Pop(Stack *s) {
+    if (s->len == 0)
+        exit(EXIT_FAILURE);
+    s->len--;
+    return (s->elements[s->len]);
+}
+
+int Top(const Stack *s) {
+    if (s->len == 0)
+        exit(EXIT_FAILURE);
+    return (s->elements[s->len - 1]);
+}
+
+_Bool IsStackEmpty(const Stack *s) {
+    if (s->len == 0)
+        return 1;
+    else
+        return 0;
+}
