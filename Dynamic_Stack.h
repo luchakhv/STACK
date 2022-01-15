c77005368db42269025313ffcad17dd4ac471da7
luchakhv <96920642+luchakhv@users.noreply.github.com>
2022-01-15 06:10:29 +0300

    Update and rename Struct.h to Dynamic_Stack.h

---
+++ Dynamic_Stack.h
@@ -0,0 +1,43 @@
+enum ERRORS {
+ STACK_OVERFLOW =404,
+ STACK_UNDERFLOW= 401,
+ OUT_OF_MEMORY= 407
+};
+
+enum VARIABLES
+{
+    MULTIPLIER = 2,
+    STACK_MAX_ELEMENTS =4,
+    ARR_INCREASE = 7,
+    ARR_REDUCE = 9
+};
+
+#ifndef STRUCTURES_STRUCT_H
+#define STRUCTURES_STRUCT_H
+
+#endif //STRUCTURES_STRUCT_H
+
+
+
+typedef int Elem;
+typedef struct stack {
+    Elem *elements;
+    Elem len;
+    Elem CurLen;
+} Stack;
+
+Stack *CreateStack();
+
+int Resize(Stack *s, int flag);
+
+int DeleteStack(Stack **s);
+
+int Push(Stack *s, int n);
+
+int Pop(Stack *s);
+
+int Peek(const Stack *s);
+
+int IsStackEmpty(const Stack *s);
+
+int ERROR(int n);
