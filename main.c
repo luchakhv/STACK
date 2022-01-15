1637d25b9f2f456d56c202007c00b2915548b995
luchakhv <96920642+luchakhv@users.noreply.github.com>
2022-01-15 06:10:47 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Struct.h"
+#include "Dynamic_Stack.h"
 #include <time.h>
 
 int main() {
     srand((unsigned int) time(0));
-    Stack *s = CreateStack();
 
+    Stack *s = CreateStack();
+    DeleteStack(&s);
+    puts("hello world");
+    s = CreateStack();
     for (int i = 0; i < 10; i++) {
         Push(s, rand() % 100);
     }
     for (int i = 0; i < 10; i++) {
         printf("%d : \n", i);
-        printf("%d\n", Top(s));
+        printf("%d\n", Peek(s));
         printf("%d\n", Pop(s));
     }
-
-
+    DeleteStack(&s);
+    puts("hello world");
     return 0;
 }
